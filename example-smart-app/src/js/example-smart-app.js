5918d813f6e0dd778a47ad3e22a0fac0b01d90c8
jeldomi <109317418+jeldomi@users.noreply.github.com>
2022-07-26 11:39:59 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -14,10 +14,11 @@
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                      code: { 'http://loinc.org|85354-9'
+                        
+                       // $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                       //       'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                        //      'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
                     }
                   });
@@ -38,8 +39,10 @@
           }
 
           var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          //var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+         // var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var systolicbp = getBloodPressureValue(byCodes('85354-9'),'8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
