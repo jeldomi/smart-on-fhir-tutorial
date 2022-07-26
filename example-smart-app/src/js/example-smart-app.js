26a0e25b399ed11c566586edb3936da891ff5fc9
jeldomi <109317418+jeldomi@users.noreply.github.com>
2022-07-26 11:49:35 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -22,8 +22,9 @@
                             // 'http://loinc.org|8462-4',
                             // 'http://loinc.org|8480-6', 
                              'http://loinc.org|2085-9', //HDL
-                             'http://loinc.org|2089-1', //LDL
-                             'http://loinc.org|55284-4']
+                             'http://loinc.org|2089-1' //LDL
+                            // 'http://loinc.org|55284-4'
+                        ]
                       }
                     }
                   });
