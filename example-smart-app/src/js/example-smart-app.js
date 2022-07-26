ad7fbc120df5cbab73a44fd93b21e4db27807ddf
jeldomi <109317418+jeldomi@users.noreply.github.com>
2022-07-26 15:24:34 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -39,12 +39,7 @@
         $.when(pt, obv,allergy).fail(onError);
 
         $.when(pt, obv,allergy).done(function(patient, obv,all) {
-          console.log("here");
-          //console.log(all);
-          console.log(all[0]);
-          console.log(all[0].code.text);
 
-          
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
